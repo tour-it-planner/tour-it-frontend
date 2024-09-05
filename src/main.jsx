8b3964707d73bf0e8ca86c5942a3932845639526
Daniela Pinto <daniela.pinto.123@hotmail.com>
2024-09-05 10:12:18 +0100

    import packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProviderWrapper } from './context/auth.context.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <AuthProviderWrapper>
+      <App />
+      </AuthProviderWrapper>
+    </BrowserRouter>
   </StrictMode>,
 )
