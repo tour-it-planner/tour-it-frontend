8632f11468fe3ccef9f87d10a55d96054fc6079a
Daniela Pinto <daniela.pinto.123@hotmail.com>
2024-09-05 15:20:50 +0100

    add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+
+    const { isLoggedIn, user, logOutuser } = useContext(AuthContext)
+
+    return (
+        <nav>
+            <Link to="/">
+                <button>Home</button>
+            </Link>
+
+            {isLoggedIn && (
+                <>
+                    <Link to="/itineraries">
+                        <button>Itineraries</button>
+                    </Link>
+                    <Link to="/destinations">
+                        <button>Destinations</button>
+                    </Link>
+
+                    <button onClick={logOutuser}>Logout</button>
+                    <span>{user && user.name}</span>
+                </>
+            )}
+
+            {!isLoggedIn && (
+                <>
+                    <Link to="/signup">
+                        {" "}
+                        <button>Sign Up</button>{" "}
+                    </Link>
+                    <Link to="/login">
+                        {" "}
+                        <button>Login</button>{" "}
+                    </Link>
+                </>
+            )}
+        </nav>
+    );
+}
+
+export default Navbar;
