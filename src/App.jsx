198c5659eaceccbc73bec65bdca2c29beb854585
Daniela Pinto <daniela.pinto.123@hotmail.com>
2024-09-05 16:12:45 +0100

    add page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom"
 import HomePage from './pages/Homepage';
+import ItineraryListPage from './pages/ItineraryListPage';
+import ItineraryDetailsPage from './pages/ItineraryDetailsPage';
+import DestinationListPage from './pages/DestinationListPage';
+import UpdateItineraryPage from './pages/UpdateItineraryPage';
+import Navbar from './components/Navbar';
 import SignupPage from './pages/SignupPage';
 import LoginPage from './pages/LoginPage';
 
@@ -10,7 +15,26 @@ function App() {
 
     <div className="App">
       <h1>Tour It</h1>
+      <Navbar/>
       <Routes>
+      <Route
+          path="/itineraries"
+          element={  <ItineraryListPage />  } 
+        />
+
+      <Route
+          path="/itineraries/:itineraryId"
+          element={ <ItineraryDetailsPage /> }
+        />
+        <Route
+          path="/destinations"
+          element={  <DestinationListPage />  } 
+        />
+
+        <Route
+          path="/itineraries/edit/:itineraryId"
+          element={  <UpdateItineraryPage />  } 
+        />
       <Route exact path="/" element={<HomePage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/login" element={<LoginPage />} />
