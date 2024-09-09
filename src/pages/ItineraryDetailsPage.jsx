9d64d2fbeb28cafd7635898b8df244d3cababfe3
Daniela Pinto <daniela.pinto.123@hotmail.com>
2024-09-09 15:19:24 +0100

    fix destinations display

--- src/pages/ItineraryDetailsPage.jsx
+++ src/pages/ItineraryDetailsPage.jsx
@@ -1,18 +1,19 @@
 import {useState, useEffect} from "react";
 import{Link, useParams} from "react-router-dom";
-import AddDestination from "../components/AddDestination";
 import DestinationCard from "../components/DestinationCard";
 import itinerariesService from "../services/itineraries.service";
 
-function ItineraryDetailsPage(props){
+function ItineraryDetailsPage(){
 
     const [itinerary , setItinerary] = useState(null);
     const { itineraryId } = useParams();
+
 
     const getItinerary = () => {
 
       itinerariesService.getItinerary(itineraryId)    
       .then((response) => {
+        console.log("Fetched itinerary: ", response.data);
         const oneItinerary = response.data;
         setItinerary(oneItinerary);
       })
@@ -29,15 +30,19 @@ function ItineraryDetailsPage(props){
         <>
           <h2>{itinerary.title}</h2>
           <p>{itinerary.description}</p>
-          <p>{itinerary.destination}</p>
+          <div className="destination-details">
+            {itinerary.destinations && itinerary.destinations.length > 0 ? (
+              itinerary.destinations.map((dest) => (
+                <DestinationCard key={dest._id} {...dest} />
+              ))
+            ) : (
+              <p>No destination selected for this itinerary.</p>
+            )}
+          </div>
         </>
       )}
 
-      
-      <AddDestination refreshItineraryt={getItinerary} itineraryId={itineraryId} />          
-
-      { itinerary && itinerary.destinations.map((destination) => <DestinationCard key={destination._id} {...destination} /> )} 
-
+    
       <Link to="/itineraries">
         <button>Back to Itineraries</button>
       </Link>
