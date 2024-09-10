2e0323fc1be40e6d6aa2c86c6b25d44c6626c801
Daniela Pinto <daniela.pinto.123@hotmail.com>
2024-09-10 14:23:20 +0100

    fix homepage import

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './index.css';
 import { Routes, Route } from "react-router-dom";
-import HomePage from './pages/HomePage';
+import HomePage from '/src/pages/HomePage.jsx';
 import ItineraryListPage from './pages/ItineraryListPage';
 import ItineraryDetailsPage from './pages/ItineraryDetailsPage';
 import DestinationListPage from './pages/DestinationListPage';
 import DestinationDetailsPage from './pages/DestinationDetailsPage';
 import UpdateItineraryPage from './pages/UpdateItineraryPage';
+import IsPrivate from './components/isPrivate';
 import Navbar from './components/Navbar';
 import SignupPage from './pages/SignupPage';
 import LoginPage from './pages/LoginPage';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -16,35 +18,26 @@ function App() {
 
     <div className="App">
       <h1>Tour It</h1>
-      <Navbar/>
+      <Navbar />
       <Routes>
-      <Route
-          path="/itineraries"
-          element={  <ItineraryListPage />  } 
-        />
+        <Route path="/itineraries" element={<ItineraryListPage />} />
 
-      <Route
-          path="/itineraries/:itineraryId"
-          element={ <ItineraryDetailsPage /> }
-        />
-        <Route
-          path="/destinations"
-          element={  <DestinationListPage />  } 
-        />
-        <Route
-          path="/destinations/:destinationId"
-          element={ <DestinationDetailsPage /> }
-        />
+        <Route path="/itineraries/:itineraryId" element={ <ItineraryDetailsPage /> } />
 
-        <Route
-          path="/itineraries/edit/:itineraryId"
-          element={  <UpdateItineraryPage />  } 
-        />
-      <Route exact path="/" element={<HomePage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/login" element={<LoginPage />} />
+        <Route path="/destinations" element={<DestinationListPage />} />
+
+        <Route path="/destinations/:destinationId" element={<DestinationDetailsPage />} />
+
+        <Route path="/itineraries/edit/:itineraryId" element={<UpdateItineraryPage />} />
+
+        <Route exact path="/" element={<HomePage />} />
+
+        <Route path="/signup" element={<SignupPage />} />
+
+        <Route path="/login" element={<LoginPage />} />
 
       </Routes>
+      <Footer />
     </div>
   );
 }
