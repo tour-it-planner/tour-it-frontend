2a9429ceb276787dda9fc473333a2b68fff3d4d4
Daniela Pinto <daniela.pinto.123@hotmail.com>
2024-09-11 16:31:48 +0100

    update text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
     
     return (
         <footer className="footer">
-        <p>&copy; 2024 Tour It  </p>
         <p> <Link to="/"><img src= "/images/homebutton.png" width="40" height="auto" align="center" />  </Link>   <Link to="https://github.com/tour-it-planner"> <img src= "/images/github-mark-white.png" width="30" height="auto" align="center" /></Link></p>
+        <p>&copy; 2024 Tour It - All rights reserved </p>
       </footer>
     );
 
