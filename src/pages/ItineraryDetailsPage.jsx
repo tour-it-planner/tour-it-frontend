bd7ae81ba2293829d54a716fb0b1181c1f0d331f
Daniela Pinto <daniela.pinto.123@hotmail.com>
2024-09-12 15:32:04 +0100

    add created date and details for itinerary

--- src/pages/ItineraryDetailsPage.jsx
+++ src/pages/ItineraryDetailsPage.jsx
@@ -1,58 +1,76 @@
-import {useState, useEffect} from "react";
-import{Link, useParams} from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 import DestinationCard from "../components/DestinationCard";
 import itinerariesService from "../services/itineraries.service";
+import moment from "moment";
 
-function ItineraryDetailsPage(){
+function ItineraryDetailsPage() {
 
-    const [itinerary , setItinerary] = useState(null);
-    const { itineraryId } = useParams();
+  const [itinerary, setItinerary] = useState(null);
+  const [formattedCreatedAt, setFormattedCreatedAt] = useState(null);
+  const { itineraryId } = useParams();
 
 
-    const getItinerary = () => {
+  const getItinerary = () => {
 
-      itinerariesService.getItinerary(itineraryId)    
+    itinerariesService.getItinerary(itineraryId)
       .then((response) => {
-        console.log("Fetched itinerary: ", response.data);
         const oneItinerary = response.data;
+
+        // calc formatted date
+        
+        const formattedResult = oneItinerary ? moment(oneItinerary.createdAt).format('DD MMMM YYYY') : '';
+        
+        // update state
+        setFormattedCreatedAt(formattedResult);
         setItinerary(oneItinerary);
       })
       .catch((error) => console.log(error));
-    };
+  };
 
-    useEffect(() => {
-        getItinerary();
-    }, []);
+  useEffect(() => {
+    getItinerary();
+  }, [itineraryId]);
 
-    return (
-        <div className="ItineraryDetails">
+
+  return (
+    <div className="ItineraryDetails">
       {itinerary && (
         <>
           <h2>{itinerary.title}</h2>
           <p>{itinerary.description}</p>
+          <ul>
+            {itinerary.details.map((e) => {
+              return(<li>{e}</li>)
+            })}
+          </ul>
+
           <div className="destination-details">
             {itinerary.destinations && itinerary.destinations.length > 0 ? (
               itinerary.destinations.map((dest) => (
                 <DestinationCard key={dest._id} {...dest} />
               ))
             ) : (
-              <p>No destination selected for this itinerary.</p>
+              <p> No destination selected for this itinerary.</p>
             )}
           </div>
+            <p>Created on: {formattedCreatedAt}</p>
         </>
       )}
 
-    
+      
+
+
       <Link to="/itineraries">
         <button>Back to Itineraries</button>
       </Link>
-          
+
       <Link to={`/itineraries/edit/${itineraryId}`}>
         <button>Edit Itinerary</button>
       </Link>
-      
+
     </div>
-    );
+  );
 
 }
 
