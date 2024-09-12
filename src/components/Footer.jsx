f0b04cb438b21c65a76d0435d26ea0dbb2fa4030
Daniela Pinto <daniela.pinto.123@hotmail.com>
2024-09-12 09:50:17 +0100

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom";
 
 function Footer() {
-    
-    return (
-        <footer className="footer">
-        <p> <Link to="/"><img src= "/images/homebutton.png" width="40" height="auto" align="center" />  </Link>   <Link to="https://github.com/tour-it-planner"> <img src= "/images/github-mark-white.png" width="30" height="auto" align="center" /></Link></p>
-        <p>&copy; 2024 Tour It - All rights reserved </p>
+  return (
+    <div className="footer-container">
+      <footer className="footer">
+        <p>
+          <Link to="/">
+            <img src="/images/homebutton.png" width="40" height="auto" alt="Home Button" />
+          </Link>
+          <a href="https://github.com/tour-it-planner">
+            <img src="/images/github-mark-white.png" width="30" height="auto" alt="GitHub" />
+          </a>
+        </p>
+        <p>&copy; 2024 Tour It - All rights reserved</p>
       </footer>
-    );
-
+    </div>
+  );
 }
+
 
 export default Footer;
