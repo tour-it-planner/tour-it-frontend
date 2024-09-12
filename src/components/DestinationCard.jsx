e3315121b5000ec24ab5c6a288f428d35de0d013
Daniela Pinto <daniela.pinto.123@hotmail.com>
2024-09-12 09:51:15 +0100

    remove description from list page

--- src/components/DestinationCard.jsx
+++ src/components/DestinationCard.jsx
@@ -8,7 +8,6 @@ import { Link } from "react-router-dom";
                 <Link to={`/destinations/${_id}`}>
                     <h3>{location}</h3>
                 </Link>
-                <p style={{ maxWidth: "400px" }}>{description} </p>
                 <img src={imageUrl}/>
             </div>
         );
